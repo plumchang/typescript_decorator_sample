f51b2794cd8103a3623ac07d01637a0707d820d2
plumchang <yuhearts.gobl@gmail.com>
2022-09-26 07:48:03 +0900

    PropertyDescriptorの確認まで

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,51 @@ function methodDecoratorFactorySample(str: string) {
   ) {
     // console.log(`メソッドデコレータが呼ばれました: ${str}`);
 
-    // target引数の確認
+    // -- target引数の確認 --
     // console.log(target);
-    // インスタンスメンバのメソッドの場合
+    // - クラスのインスタンス化（インスタンスメンバのメソッドの場合）
     // var tmp = new target.constructor("こんばんは");
     // tmp.printStr();
-    // staticメソッドの場合
+    // - クラスのインスタンス化（staticメソッドの場合）
     // var tmp = new target("こんばんは");
     // tmp.printStr();
 
-    // propertyKeyの確認
-    console.log(propertyKey);
+    // -- propertyKeyの確認 --
+    // console.log(propertyKey);
 
-    //descriptor.value = () => {
-    //console.log(`対象のメソッドが呼ばれました: ${str}`);
-    //};
+    // -- descriptorの確認 --
+    console.log(descriptor);
+    // - configurableの確認
+    // descriptor.configurable = false;
+    // - enumerableの確認
+    // descriptor.enumerable = true;
+    // - valueの確認
+    // descriptor.value();
+    // - writableの確認
+    // descriptor.writable = false;
+    // -- メソッドの書き換え --
+    // descriptor.value = () => {
+    // console.log(`対象のメソッドが呼ばれました: ${str}`);
+    // };
+    // - get・setの確認
+    var orgMethod = descriptor.value;
+    var retDesc = {
+      configurable: true,
+      enumerable: false,
+      get() {
+        console.log("getter called");
+        return orgMethod;
+      },
+      set(value: any) {
+        console.log("setter called");
+        orgMethod = value;
+      },
+    };
+    return retDesc;
   };
 }
 
+// サンプルクラス
 class SampleClass {
   str: string;
   constructor(str: string) {
@@ -35,16 +62,50 @@ class SampleClass {
     this.str = str;
   }
 
-  @methodDecoratorFactorySample("")
+  //@methodDecoratorFactorySample("static method")
   static printHello() {
     console.log("Hello from static method");
   }
 
-  @methodDecoratorFactorySample("おはよう")
+  @methodDecoratorFactorySample("instance method")
   printStr() {
     console.log(this.str);
   }
 }
 
-// var sampleClass = new SampleClass("こんにちは");
-//sampleClass.printStr();
+var sampleClass = new SampleClass("こんにちは");
+
+// PropertyDescriptorの出力（デコレータで変更された後の値確認）
+console.log(
+  Object.getOwnPropertyDescriptor(
+    Object.getPrototypeOf(sampleClass),
+    "printStr"
+  )
+);
+
+// configurableの確認（メソッドを削除できるかどうか）
+// delete Object.getPrototypeOf(sampleClass).printStr;
+// sampleClass.printStr();
+
+// enumerableの確認
+// for (var key in sampleClass) {
+// console.log(`要素の列挙：${key}`);
+// }
+
+// valueの確認
+// sampleClass.printStr();
+
+// writableの確認（メソッドの内容変更）
+// sampleClass.printStr = () => {
+//   console.log("changed");
+// };
+// sampleClass.printStr();
+
+// getの確認
+// sampleClass.printStr();
+
+// setの確認
+sampleClass.printStr = () => {
+  console.log("changed by setter");
+};
+sampleClass.printStr();
